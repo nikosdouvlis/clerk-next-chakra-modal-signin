edb9e1677778676b59b45ce13128428e45e9431c
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-02-04 18:07:14 +0200

    Configure app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ClerkProvider>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </ClerkProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
